737653f7ffc4d0608d5378c523a10c619015a32c
LakshayySharma <44471078+LakshayySharma@users.noreply.github.com>
2022-12-18 19:04:29 +0530

    initial commit

---
+++ public/main.js
@@ -0,0 +1,34 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+const isDev = require("electron-is-dev");
+function createWindow() {
+  // Create the browser window.
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  // and load the index.html of the app.
+  win.loadURL(
+    isDev
+      ? "http://localhost:3000"
+      : `file:/${path.join(__dirname, "..build/index.html")}/`
+  );
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
